34c8b49fa36a7e1a7624462f499dc87cff7d8061
Kazuki Kikuchi <ery.eden@gmail.com>
2015-12-08 15:15:54 +0900

    add smooth scroll

--- init.el
+++ init.el
@@ -90,6 +90,20 @@
 ;;Buffer Cycling
 (require 'buffcycle)
 
+;; Smooth scroll
+;; (require 'smooth-scroll)
+;; (smooth-scroll-mode t)
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed 'nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+;; (setq scroll-step 1) ;; keyboard scroll one line at a time
+;; (setq scroll-conservatively 0)
+;; (setq scroll-margin 10)
+(setq-default smooth-scroll-margin 0)
+(setq scroll-step 1
+      scroll-margin 1
+      scroll-conservatively 100000)
 
 
 
